4ce714608ceb5e6603708f7805259899051a990f
abbosxolliyev9-tech <abbosxolliyev9@gmail.com>
2026-04-26 00:43:09 +0500

    Update logic.py

--- calculations/logic.py
+++ calculations/logic.py
@@ -1,21 +1,22 @@
-import re
 import math
+import re
 
 def get_pack_size(name):
     name_str = str(name).upper()
     match = re.search(r'[N№](\d+)', name_str)
     return int(match.group(1)) if match else 1
 
-def calculate_logic(cost, mode="admin", user_markup=10, pack_size=1):
+def calculate_logic(cost, mode, markup_val=10):
     if cost <= 0: return 0, 0
-    if mode == "admin":
-        markup = 1.08 if cost >= 300000 else 1.10
-        round_val = 50
-    else:
-        markup = 1 + (user_markup / 100)
-        round_val = 100
     
+    # 1. Admin: >300k бўлса 8%, <300k бўлса 10%
+    if mode == "Админ Ҳисоб":
+        markup = 1.08 if cost >= 300000 else 1.10
+    # 2. Mijoz: Фоизни ўзи белгилайди
+    else:
+        markup = 1 + (markup_val / 100)
+        
     pachka_raw = cost * markup
-    pachka_final = math.ceil(pachka_raw / round_val) * round_val
-    dona_final = math.ceil((pachka_final / pack_size) / 100) * 100
-    return int(pachka_final), int(dona_final)
+    # 100 сўмга яхлитлаш
+    pachka_final = math.ceil(pachka_raw / 100) * 100
+    return int(pachka_final)
